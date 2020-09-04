0f7294e94dd68c3bfb88e0eb1f8544def1b2a240
Ryan Rosiak <ryan11291129@gmail.com>
2020-09-04 10:48:08 -0400

    Added lab1 document, need to start and complete

--- Test/hello.c
+++ Test/hello.c
@@ -8,7 +8,21 @@ int main(int argc, char** argv) {
 
     MPI_Comm world = MPI_COMM_WORLD; // This constant gets set by the MPI lib
 
-    puts("Hello World!");
+    // worldSize will be the total number of nodes in the communicator
+    // myRank will be the node's id within that communicator
+    int worldSize, myRank;
+
+    MPI_Comm_size(world, &worldSize);
+    MPI_Comm_rank(world, &myRank);
+
+    // This will only execute on the "master" node
+    if (myRank == 0) {
+        printf("Hello from the master node!\n");
+        printf("There are %d total nodes.\n", worldSize);
+    } else {
+        // Executed in worker nodes
+        printf("Hello, from the worker node %d!\n", myRank);
+    }
 
     MPI_Finalize();
 
