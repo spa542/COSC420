529721afd418428c5cfccf8ed2fe921c263d08a1
Ryan Rosiak <ryan11291129@gmail.com>
2020-10-05 20:02:22 -0400

    added project changes

--- Project-1-ProjectEuler/Problem282Rosiak/main.c
+++ Project-1-ProjectEuler/Problem282Rosiak/main.c
@@ -9,26 +9,78 @@
 
 // Knuth Arrow function
 unsigned long long knuthArrow(unsigned long long a, unsigned long long b) {
-    a %= reduce;
-    b %= reduce;
-    //return ((unsigned long long)pow(a, b)) % reduce;
+    //a %= reduce;
+    //b %= reduce;
 
     unsigned long long n = a;
-    int limit = 0;
-    while (limit < b) {
+    unsigned long long limit = 0;
+    while (limit < b - 1) {
         if (n > reduce) {
             n %= reduce;
         }
         n *= a;
         limit++;
     }
+    n %= reduce;
+    printf("Result of knuth arrow: %lld\n", n);
+    return n;
+}
+
+unsigned long long knuthArrow2(unsigned long long a, unsigned long long b) {
+    unsigned long long copies = knuthArrow(a,b);
+    printf("Copies knuth arrow 2 = %lld\n", copies);
+    unsigned long long limit = 0;
+    unsigned long long n = a;
+    while (limit < copies - 1) {
+        if (n > reduce) {
+            n %= reduce;
+        }
+        n *= a;
+        limit++;
+    }
+    n %= reduce;
+    printf("Result of knuth arrow 2: %lld\n", n);
+    return n;
+}
+
+unsigned long long knuthArrow3(unsigned long long a, unsigned long long b) {
+    unsigned long long copies = knuthArrow2(a,b); 
+    printf("Copies knuth arrow 3 = %lld\n", copies);
+    unsigned long long limit = 0;
+    unsigned long long n = a;
+    while (limit < copies) {
+        if (n > reduce) {
+            n %= reduce;
+        }
+        n *= a;
+        limit++;
+    }
+    n %= reduce;
+    printf("Result of knuth arrow 3: %lld\n", n);
+    return n;
+}
+
+unsigned long long knuthArrow4(unsigned long long a, unsigned long long b) {
+    unsigned long long copies = knuthArrow3(a,b); 
+    printf("Copies knuth arrow 4 = %lld\n", copies);
+    unsigned long long limit = 0;
+    unsigned long long n = a;
+    while (limit < copies - 1) {
+        if (n > reduce) {
+            n %= reduce;
+        }
+        n *= a;
+        limit++;
+    }
+    n %= reduce;
+    printf("Result of knuth arrow 4: %lld\n", n);
     return n;
 }
 
 // Ackermann function using closed forms
 unsigned long long ack(unsigned long long m, unsigned long long n) {
-    m %= reduce; // reduce the number by mod 14^8
-    n %= reduce; // reduce the number by mod 14^8
+    //m %= reduce; // reduce the number by mod 14^8
+    //n %= reduce; // reduce the number by mod 14^8
     /*
     push(top, m);
     while(count(top) != 0) {
@@ -73,11 +125,11 @@ unsigned long long ack(unsigned long long m, unsigned long long n) {
         n = pow(2, n + 3) - 3;
         n %= reduce;
     } else if (m == 4) {
-        n = knuthArrow(2, knuthArrow(2, n + 3)) - 3; 
+        n = ((knuthArrow2(2, n + 3) % reduce) - (3 % reduce)) % reduce;
     } else if (m == 5) {
-        n = knuthArrow(2, knuthArrow(2, knuthArrow(2, n + 3))) - 3; 
-    } else {
-        n = knuthArrow(2, knuthArrow(2, knuthArrow(2, knuthArrow(2, n + 3)))) - 3; 
+        n = ((knuthArrow3(2, n + 3) % reduce) - (3 % reduce)) % reduce;
+    } else if (m == 6) {
+        n = ((knuthArrow4(2, n + 3) % reduce) - (3 % reduce)) % reduce;
     }
     return n;
 }
@@ -87,7 +139,12 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     //stackPtr newptr = NULL; // Declare a pointer that starts the top of the stack
-    
+   /* 
+    printf("2up->4 = %lld\n", knuthArrow(2,4));
+    printf("2up->up->4 = %lld\n", knuthArrow2(2,4));
+    printf("2up->up->up->4 = %lld\n", knuthArrow3(2,4));
+    printf("2up->up->up->up->4 = %lld\n", knuthArrow4(2,4));
+
     printf("A(1,0) = %lld\n", ack(1,0));
 
     printf("A(2,2) = %lld\n", ack(2,2));
@@ -100,21 +157,17 @@ int main(int argc, char** argv) {
 
     printf("A(4,4) = %lld\n", ack(4,4));
 
-    //printf("A(5,5) = %lld\n", ack(5,5));
+    printf("A(5,5) = %lld\n", ack(5,5));
 
-    //printf("A(6,6) = %lld\n", ack(6,6));
-
-    unsigned long long sum = 0, tmp = 0;
+    printf("A(6,6) = %lld\n", ack(6,6));
+    */
+    unsigned long long sum = 0;
     int i;
     for (i = 0; i <= 6; i++) {
-        if (i == 0) {
-            sum = ack(i,i);
-        } else {
-            tmp = (sum % reduce) + (ack(i,i) % reduce);
-            sum = tmp % reduce;
-        }
+        sum += (ack(i,i) % reduce);
     }
-    printf("sum: %lld\n", sum % reduce);
+    sum %= reduce;
+    printf("sum: %lld\n", sum);
 
     MPI_Finalize();
 
