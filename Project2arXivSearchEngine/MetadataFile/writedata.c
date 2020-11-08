a2b0893b46496f7938320e9288d6b5d1ff6cd410
Ryan Rosiak <ryan11291129@gmail.com>
2020-11-08 17:24:37 -0500

    Added sample bst, need to make tests and fix the problem with the previous scripts

--- Project2arXivSearchEngine/MetadataFile/writedata.c
+++ Project2arXivSearchEngine/MetadataFile/writedata.c
@@ -2,12 +2,12 @@
 #include<stdlib.h>
 #include<string.h>
 
-typedef struct MetaNode {
+typedef struct MetaDataNode {
     char id[18];
     char title[384];
     char author[36664];
     char abstract[6093];
-} mnode;
+} mdnode;
 
 #define FILE_LINES 8140940
 
@@ -30,19 +30,36 @@ int main() {
     int cap = FILE_LINES / 5;
     printf("# of papers: %d\n", cap);
     puts("BEFORE mnode");
-    mnode arr[cap];
+    //mnode *arr = malloc(cap*sizeof(mnode));
+    mdnode arr[cap];
     puts("After mnode");
-    memset(arr, 0, cap*sizeof(mnode));
+    memset(arr, 0, cap*sizeof(mdnode));
     char tmp[10];
     puts("BEFORE LOOP");
     while (is < 1) {
-        mnode newNode;
+        mdnode newNode;
         fgets(newNode.id, 18, fh);
         fgets(newNode.title, 384, fh);
         fgets(newNode.author, 36664, fh);
         fgets(newNode.abstract, 6093, fh);
         fgets(tmp, 10, fh);
-        arr[is] = newNode;
+        /*
+        for (i = 0; i < 1; i++) {
+            printf("ID: %s\n", newNode.id);
+            printf("Title: %s\n", newNode.title);
+            printf("Author(s): %s\n", newNode.author);
+            printf("Abstract: %s\n", newNode.abstract);
+        }
+        */
+        puts("Breaks?");
+        strcpy(arr[is].id, newNode.id);
+        puts("huh?");
+        strcpy(arr[is].title, newNode.title);
+        puts("what?");
+        strcpy(arr[is].author, newNode.author);
+        puts("why");
+        strcpy(arr[is].abstract, newNode.abstract);
+        puts("problem?");
         is++;
     }
     puts("AFTER LOOP");
@@ -53,6 +70,7 @@ int main() {
         printf("Abstract: %s\n", arr[i].abstract);
     }
 
+    free(arr);
     fclose(fh);
 
     return 0;
